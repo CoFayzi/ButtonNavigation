458933f6a56225d2cbfd6d2826d6d10f6048e464
Fayzullo <abdulazizovfayzullo0@gmail.com>
2022-07-02 07:14:22 +0500

    Hello

---
+++ app/src/main/java/com/example/buket/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.buket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val likeFragment = LikeFragment()
+        val searchFragment = SearchFragment()
+        val shopFragment = ShopFragment()
+
+    }
+    
+
+
+
+
+        }
